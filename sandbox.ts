531b1f583d8a69a37431c88c76696ca3b4ae0794
Shaun <shaun@thenetninja.co.uk>
2020-06-02 10:14:21 +0100

    lesson-4

--- sandbox.ts
+++ sandbox.ts
@@ -1,29 +1,22 @@
-// // uuid module
-import { v4 } from "https://deno.land/std/uuid/mod.ts";
-
-const uid = v4.generate();
-console.log(uid);
-
-// // fs module
-import { readJson, writeJson } from "https://deno.land/std/fs/mod.ts";
-
-const jsonObj = await readJson('ninjas.json');
-console.log(jsonObj);
-
-const books = [
-  { title: 'the way of kings', author: 'brandon sanderson' },
-  { title: 'name of the wind', author: 'patrick rothfuss' },
-];
-
-await writeJson('books.json', books, { spaces: 2 });
-console.log('created books.json');
-
 // http module (server)
-import { serve } from "https://deno.land/std/http/server.ts";
+// import { serve } from "https://deno.land/std/http/server.ts";
 
-const server = serve({ port: 3000 });
-console.log('listening for requests on port 3000');
+// const server = serve({ port: 3000 });
+// console.log('listening for requests on port 3000');
 
-for await (const req of server) {
-  req.respond({ body: `Hello again Ninjas, you visited ${req.url}` });
-}
+// for await (const req of server) {
+//   console.log('request made');
+//   req.respond({ body: `Hello again Ninjas, you visited ${req.url}` });
+// }
+
+// case module
+import { camelCase, paramCase, pascalCase, snakeCase } from "https://deno.land/x/case/mod.ts";
+
+const text = 'hello again ninjas';
+
+const camel = camelCase(text);
+const param = paramCase(text);
+const pascal = pascalCase(text);
+const snake = snakeCase(text);
+
+console.log(camel, param, pascal, snake);
